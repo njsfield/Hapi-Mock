096f8da46c0074f6b1050cbea1967270d8d9128d
Nick Field <heyimnick@live.com>
2016-11-19 13:38:40 +0000

    Update mock.js

--- mock.js
+++ mock.js
@@ -1,19 +1,3 @@
-/* 
-In Hapi, routes that match paths with higher specificity win...
-
-Route 1 : path = '/{stuff*}',
-Route 2 : path = '/files/{file}/me.jpg',
-
-Get request Path = '/file/nick/me.jpg'
-
-Winner = Route 2.
-
-This demonstrates how routes can be compared against a live get request path to match the more specific route.
-Uses a fake .get method (like server.inject)
-
-*/
-
-
 //// Hapi Server Mock
 
 function Server (){
@@ -95,3 +79,5 @@ Server.prototype.get = function(path){
     });
     return result;
 }
+
+module.exports = Server;
